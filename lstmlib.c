ea998a26f1ded16cc88f50744ba7e3d809d5f67f
az13js <1654602334@qq.com>
2019-05-13 22:32:54 +0800

    编写lstmlib_create

--- lstmlib.c
+++ lstmlib.c
@@ -11,9 +11,75 @@ struct lstmlib* lstmlib_create(int length)
     if (!unit) {
         return NULL;
     }
+    (*unit).error_no = 0;
+    (*unit).error_msg = "\0";
     (*unit).length = length;
     (*unit).x = (double*)calloc(length, sizeof (double));
     if (NULL == (*unit).x) {
+        free(unit);
+        return NULL;
+    }
+    (*unit).h = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).h) {
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).f = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).f) {
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).i = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).i) {
+        free((*unit).f);
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).tilde_C = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).tilde_C) {
+        free((*unit).i);
+        free((*unit).f);
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).C = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).C) {
+        free((*unit).tilde_C);
+        free((*unit).i);
+        free((*unit).f);
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).o = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).o) {
+        free((*unit).C);
+        free((*unit).tilde_C);
+        free((*unit).i);
+        free((*unit).f);
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
+        return NULL;
+    }
+    (*unit).hat_h = (double*)calloc(length, sizeof (double));
+    if (NULL == (*unit).hat_h) {
+        free((*unit).o);
+        free((*unit).C);
+        free((*unit).tilde_C);
+        free((*unit).i);
+        free((*unit).f);
+        free((*unit).h);
+        free((*unit).x);
+        free(unit);
         return NULL;
     }
     return unit;
