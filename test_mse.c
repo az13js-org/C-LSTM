b5871ba0450bd3ddb4f7493c52168188bbff530e
az13js <1654602334@qq.com>
2019-05-14 22:33:37 +0800

    MSE

--- test_mse.c
+++ test_mse.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     int i, length = 100;
     unit = lstmlib_create(length);
     for (i = 0; i < length; i++) {
-        (*unit).h[i] = 1.0;
-        (*unit).hat_h[i] = 0.0;
+        (*unit).h[i] = 2.0;
+        (*unit).hat_h[i] = -1.0;
     }
     printf("MSE:%lf\n", lstmlib_get_mse(unit));
     return 0;
