dbfc73dc1fc1556f9e5b879f27d16bc4573491e0
az13js <1654602334@qq.com>
2019-05-19 14:20:33 +0800

    转移源码到src

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-gcc -O3 lstmlib.c test.c -lm -o test.out
+gcc -O3 src/lstmlib.c src/test.c -lm -o test.out
 ./test.out
 
-gcc -O3 lstmlib.c test_create.c -lm -o test_create.out
+gcc -O3 src/lstmlib.c src/test_create.c -lm -o test_create.out
 ./test_create.out
 
-gcc -O3 lstmlib.c test_run.c -lm -o test_run.out
+gcc -O3 src/lstmlib.c src/test_run.c -lm -o test_run.out
 ./test_run.out
 
-gcc -O3 lstmlib.c test_mse.c -lm -o test_mse.out
+gcc -O3 src/lstmlib.c src/test_mse.c -lm -o test_mse.out
 ./test_mse.out
 
-gcc -O3 lstmlib.c test_fitunit.c -lm -o test_fitunit.out
+gcc -O3 src/lstmlib.c src/test_fitunit.c -lm -o test_fitunit.out
 ./test_fitunit.out
